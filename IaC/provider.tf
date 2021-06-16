3a64a0a149e0a0ba3bb4f0ceae7a45687ecd8a53
Kenny White <kennywhite1@gmail.com>
2021-06-16 13:32:48 +0200

    setting up terrafrom init in pipeline, configured basic azurerm provider

---
+++ IaC/provider.tf
@@ -0,0 +1,19 @@
+# Lock in the specify the provider version
+terraform {
+
+  #version of the terraform executable
+  required_version = "1.0.0"
+
+  #version of the azurerm provider
+  required_providers {
+    azurerm = {
+      version = ">= 2.63.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
