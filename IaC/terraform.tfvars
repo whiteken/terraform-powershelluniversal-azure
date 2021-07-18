31ec37a46ed1ff50461c765512a5ca292343d98a
Kenny White <kennywhite1@gmail.com>
2021-07-18 15:46:36 +0200

    moved variables into terraform cloud portal

--- IaC/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-azure_region       = "west europe"
-azure_region_short = "we"
-project_name       = "PowerShellUniversal-terraform"
-my_public_domain   = "terraformlabs.net"
--- IaC/webapp.tf
+++ IaC/webapp.tf
@@ -2,7 +2,6 @@ resource "azurerm_resource_group" "rg" {
   name     = "devtest-${var.project_name}-${var.azure_region_short}-rg1"
   location = var.azure_region
 }
-
 resource "azurerm_app_service_plan" "myplan" {
   name                = var.project_name
   location            = azurerm_resource_group.rg.location
@@ -22,7 +21,7 @@ resource "azurerm_app_service" "main" {
 
   site_config {
     dotnet_framework_version  = "v4.0"
-    managed_pipeline_mode = "Integrated"
+    managed_pipeline_mode     = "Integrated"
     websockets_enabled        = true
     ftps_state                = "AllAllowed"
     use_32_bit_worker_process = false
